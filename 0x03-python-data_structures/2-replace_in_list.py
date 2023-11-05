60148f7cdd2f6a56153d0d5b459a834ce3728d6c
Boda <bodaa2300@gmail.com>
2023-11-05 15:39:07 -0800

    a

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to replace an element from a list at a specific position like in C
-#
-# -----------------------------------------------------------
+# 2-replace_in_list.py
 
 
 def replace_in_list(my_list, idx, element):
-    """Replaces an element of a list at a specific position
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
 
-    Args:
-        my_list: a list
-        idx: the index of item to replace
-        element: item to be substituted
-
-    Returns:
-        the edited list
-    """
-
-    # Check for negative and out of range index
-    if idx < 0:
-        return my_list
-    if idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to removes all characters c and C from a string
-
+# 5-no_c.py
 
 
 def no_c(my_string):
-    """Removes all characters c and C from a string
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
 
-    Args:
-        my_list: a string
-
-    Returns:
-        the new string
-    """
-
-    # Create a mapping table(dictionary) that sets the characters
-    # to their replacement values
-    dict = {ord(char): None for char in "cC"}
-
-    # Call the translate string method giving it the mapping table
-    new_string = my_string.translate(dict)
-    return new_string
